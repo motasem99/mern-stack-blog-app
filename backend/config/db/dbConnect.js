08727e56e92861218af15665e5f9efbc3cfe29ce
mutasem <motasemimad10@gmail.com>
2022-09-21 12:54:48 +0300

    Environment Variable DOTENV

--- backend/config/db/dbConnect.js
+++ backend/config/db/dbConnect.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose');
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(
-      'mongodb+srv://bloguser:XHyHYb2ySOt9Z8YF@mern-blog-app.e74yuxg.mongodb.net/?retryWrites=true&w=majority',
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
     console.log('Db is connected successfully');
   } catch (err) {
     console.log(`Error ${err.message}`);
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
 const dbConnect = require('./config/db/dbConnect');
 const app = express();
 
 // DB
 dbConnect();
+console.log(process.env);
 
 // server
 const PORT = process.env.PORT || 5000;
